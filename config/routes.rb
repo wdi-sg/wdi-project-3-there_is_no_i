1072fdf209b42dc05499149fbba44ead8af641aa
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-04-29 12:49:43 +0800

    add links to test credit card

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # routes for Stripe credit cards charges
+  root 'home#home'
   resources :charges
 end
