fa3fe003da4202d75acf68d3dc8e9b67b2158655
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-05-01 12:50:49 +0800

    restaurant seed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,11 +2,11 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address1, presence: true
   validates :address_city, presence: true
-  validates :address_state, presence: true
+  # validates :address_state, presence: true
   validates :address_country, presence: true
-  validates :address_postal, presence: true
+  # validates :address_postal, presence: true
   validates :description, presence: true
   validates :cuisine, presence: true
   validates :email, presence: true
-  validates :password, presence: true
+  # validates :password, presence: true
 end
