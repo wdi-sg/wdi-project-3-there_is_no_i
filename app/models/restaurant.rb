d9dc82a5c0098b73e863c271fd18de5fa021413b
Darrell Teo <darrelltzj@gmail.com>
2017-05-01 16:52:44 +0800

    seed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,7 +9,7 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address1, presence: true
   validates :address_city, presence: true
-  validates :address_state, presence: true
+  # validates :address_state, presence: true
   validates :address_country, presence: true
   validates :address_postal, presence: true
   validates :description, presence: true
