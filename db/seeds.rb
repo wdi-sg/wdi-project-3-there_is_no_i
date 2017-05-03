8f6e5cea8c7f94bf6248828413c47871320e1ffc
Darrell Teo <darrelltzj@gmail.com>
2017-05-03 21:17:21 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,13 +40,79 @@ end
   )
 end
 
-500.times do
-  Table.create(
-    restaurant_id: rand(100) + 1,
-    name: rand(10).to_s,
-    capacity_current: 0,
-    capacity_total: rand(10) + 1
-  )
+# 500.times do
+#   Table.create(
+#     restaurant_id: rand(100) + 1,
+#     name: rand(10).to_s,
+#     capacity_current: 0,
+#     capacity_total: rand(10) + 1
+#   )
+# end
+
+@restaurants = Restaurant.all
+@restaurants.each do |restaurant|
+  Table.create([
+                 {
+                   restaurant_id: restaurant.id,
+                   name: 1,
+                   capacity_current: 2,
+                   capacity_total: 2
+                 },
+                 {
+                   restaurant_id: restaurant.id,
+                   name: 2,
+                   capacity_current: 0,
+                   capacity_total: 2
+                 },
+                 {
+                   restaurant_id: restaurant.id,
+                   name: 3,
+                   capacity_current: 0,
+                   capacity_total: 2
+                 },
+                 {
+                   restaurant_id: restaurant.id,
+                   name: 4,
+                   capacity_current: 4,
+                   capacity_total: 4
+                 },
+                 {
+                   restaurant_id: restaurant.id,
+                   name: 5,
+                   capacity_current: 3,
+                   capacity_total: 4
+                 },
+                 {
+                   restaurant_id: restaurant.id,
+                   name: 6,
+                   capacity_current: 0,
+                   capacity_total: 4
+                 },
+                 {
+                   restaurant_id: restaurant.id,
+                   name: 7,
+                   capacity_current: 6,
+                   capacity_total: 6
+                 },
+                 {
+                   restaurant_id: restaurant.id,
+                   name: 8,
+                   capacity_current: 0,
+                   capacity_total: 6
+                 },
+                 {
+                   restaurant_id: restaurant.id,
+                   name: 9,
+                   capacity_current: 7,
+                   capacity_total: 8
+                 },
+                 {
+                   restaurant_id: restaurant.id,
+                   name: 10,
+                   capacity_current: 0,
+                   capacity_total: 8
+                 }
+               ])
 end
 
 500.times do
