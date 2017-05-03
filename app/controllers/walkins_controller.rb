38809ad8049e922847df6e46fe63fe2c67153077
Darrell Teo <darrelltzj@gmail.com>
2017-05-04 03:30:27 +0800

    table assignment for walkin

--- app/controllers/walkins_controller.rb
+++ app/controllers/walkins_controller.rb
@@ -1,5 +1,5 @@
 class WalkinsController < ApplicationController
-  before_action :set_restaurant, only: %i[index new create public_new public_create public_show edit update destroy]
+  before_action :set_restaurant, only: %i[index new main_create create public_new public_create public_show edit update destroy]
   before_action :set_walkin, only: %i[show edit update destroy]
   # before_action :find_queue_number, only: %i[create]
   helper WalkinHelper
@@ -9,10 +9,7 @@ class WalkinsController < ApplicationController
   end
 
   def create
-    @walkin = Reservation.new(walkin_params)
-    @walkin.restaurant_id = @restaurant.id
-    @walkin.queue_number = find_queue_number
-    set_restaurant_queue()
+    main_create(walkin_params)
     if @walkin.save!
       redirect_to restaurant_walkins_path(@restaurant)
     else
@@ -25,16 +22,45 @@ class WalkinsController < ApplicationController
   end
 
   def public_create
-    @walkin = Reservation.new(public_params)
-    @walkin.restaurant_id = @restaurant.id
-    @walkin.status = 'queuing'
-    @walkin.queue_number = find_queue_number
-    set_restaurant_queue
+    main_create(public_params)
     set_walkin_user(@walkin)
-    if @walkin.save!
-      redirect_to restaurant_public_path(@restaurant)
+    find_aval_tables
+    if @aval_tables.length > 0
+      determine_table(@aval_tables, @walkin)
+# ====================================
+# CHANGE THIS ONCE determine_table IS SETTLED
+      # @walkin.status = 'queuing'
+      # if @walkin.save!
+      #   redirect_to restaurant_public_path(@restaurant)
+      # else
+      #   render :public_new
+      # end
+# ====================================
+      if @chosen_table
+        @walkin.table_id = @chosen_table.id
+        # CHANGE TABLE CURRENT CAPACITY !!!!
+        change_table_status(@chosen_table, @walkin.party_size)
+        @walkin.status = 'dining'
+        if @walkin.save!
+          redirect_to restaurant_public_path(@restaurant)
+        else
+          render :public_new
+        end
+      else
+        @walkin.status = 'queuing'
+        if @walkin.save!
+          redirect_to restaurant_public_path(@restaurant)
+        else
+          render :public_new
+        end
+      end
     else
-      render :new
+      @walkin.status = 'queuing'
+      if @walkin.save!
+        redirect_to restaurant_public_path(@restaurant)
+      else
+        render :public_new
+      end
     end
   end
 
@@ -97,6 +123,81 @@ class WalkinsController < ApplicationController
 
   def set_walkin
     @walkin = Reservation.find(params[:id])
+  end
+
+  def main_create(params)
+    @walkin = Reservation.new(params)
+    @walkin.restaurant_id = @restaurant.id
+    @walkin.queue_number = find_queue_number
+    set_restaurant_queue
+  end
+
+  def find_aval_tables
+    set_restaurant
+    @aval_tables = Table.where(restaurant_id: @restaurant.id).where('capacity_current = ?', 0)
+  end
+
+  def determine_table(aval_tables, walkin_person)
+    block = 2.hours
+    r_start_time = Time.now
+    r_end_time = r_start_time + block
+    date = r_start_time.strftime("%Y-%m-%d")
+
+    # This only shows reservations (not tables avail)
+    all_reservations = Reservation.where(restaurant_id: params[:restaurant_id]).where("DATE(start_time) = ?", date)
+
+    # Q: If there are no reservations, there are no tables?
+    # all_avail_reservations = all_reservations.where("start_time >= ?", r_end_time).or(all_reservations.where("end_time <= ?", r_start_time)).to_a
+
+    #New Method
+    # 1) @avail tables
+    # 2) Find Blocked tables of blocked reservations
+    # 3) Filter off tables from blocked
+    # 4) Filter off tables of capacity_total
+    # 5) sort
+    # 6) Choose
+
+    affecting_reservations = all_reservations.where("start_time < ?", r_end_time - block).or(all_reservations.where("end_time > ?", r_start_time)).to_a
+
+    affected_tables = []
+    affecting_reservations.each do |reservation|
+      affected_tables.push(Table.where("table_id = ?", reservation.table_id))
+    end
+    
+    p '===AFFECTED==='
+    p affected_tables
+
+    if affected_tables.length > 0
+      affected_tables.map do |table|
+        table.id
+      end
+    end
+
+    aval_tables.select do |table|
+      affected_tables.exclude?(table[:id])
+    end
+
+    p '====AVAL TABLES====='
+    p aval_tables
+
+    filtered_aval_tables = []
+    aval_tables.each do |table|
+      if table.capacity_total >= walkin_person.party_size
+        filtered_aval_tables.push(table)
+      end
+    end
+
+    filtered_aval_tables.sort_by! {|table| table.capacity_total }
+
+    p '====FILTERED TABLES====='
+    p filtered_aval_tables
+
+    @chosen_table = filtered_aval_tables[0]
+  end
+
+  def change_table_status(table, filled)
+    table.capacity_current = filled
+    table.save!
   end
 
   def public_params
