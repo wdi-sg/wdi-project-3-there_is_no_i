696a196fdf22fb2f218e1d327f63fad761fb7724
Lou Lee <louisa.leeyh@gmail.com>
2017-05-04 22:21:18 +0800

    added sorting for menu items

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -7,7 +7,20 @@ class MenuItemsController < ApplicationController
   helper MenuItemsHelper
 
   def index
-    @menu_items = MenuItem.where(restaurant_id: params[:restaurant_id]).order('LOWER(name) ASC')
+    @restaurant_id = params[:restaurant_id]
+    if request.fullpath == "/restaurants/#{@restaurant_id}/menu_items?name=sort"
+      @menu_items = MenuItem.where(restaurant_id: params[:restaurant_id]).order(:name)
+    elsif request.fullpath == "/restaurants/#{@restaurant_id}/menu_items?price=sort"
+      @menu_items = MenuItem.where(restaurant_id: params[:restaurant_id]).order(:price)
+    elsif request.fullpath == "/restaurants/#{@restaurant_id}/menu_items?description=sort"
+      @menu_items = MenuItem.where(restaurant_id: params[:restaurant_id]).order(:description)
+    elsif request.fullpath == "/restaurants/#{@restaurant_id}/menu_items?ingredient=sort"
+      @menu_items = MenuItem.where(restaurant_id: params[:restaurant_id]).order(:ingredients)
+    elsif request.fullpath == "/restaurants/#{@restaurant_id}/menu_items?tag=sort"
+      @menu_items = MenuItem.where(restaurant_id: params[:restaurant_id]).order(:tags)
+    else
+      @menu_items = MenuItem.where(restaurant_id: params[:restaurant_id]).order('LOWER(name) ASC')
+    end
   end
 
   def create
