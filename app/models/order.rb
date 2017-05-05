60d6dc3b604b69083cec464f8a0ed34d311dac8e
Darrell Teo <darrelltzj@gmail.com>
2017-05-05 17:19:50 +0800

    temp

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   # belongs_to :menu_items
   # HEROKU ERROR: "tried to define an association named transaction on the model Order, but this will conflict with a method transaction already defined by Active Record"
-  belongs_to :transaction
+  # belongs_to :transaction
   # belongs_to :user
   # belongs_to :restaurant
 
