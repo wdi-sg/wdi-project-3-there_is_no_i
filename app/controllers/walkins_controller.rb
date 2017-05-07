63168882c93098334f352e4dfede9cc3d217718e
Darrell Teo <darrelltzj@gmail.com>
2017-05-07 20:21:36 +0800

    working on diner edit

--- app/controllers/walkins_controller.rb
+++ app/controllers/walkins_controller.rb
@@ -1,5 +1,6 @@
 class WalkinsController < ApplicationController
   include FindingTableLogic
+  include Sms
   before_action :authenticate_user!, except: [:new]
   before_action :set_restaurant, only: %i[index new create_walkin create public_new public_create public_save public_show edit update destroy]
   before_action :set_walkin, only: %i[show edit update destroy]
@@ -141,18 +142,6 @@ class WalkinsController < ApplicationController
     end
     if new_end_time
       customer.end_time = new_end_time
-    end
-  end
-
-  def sms_awaiting(walkin_name, restaurant_name, chosen_table_name)
-    p "SMS: Dear #{walkin_name}, your reservation at #{restaurant_name} is ready. Please proceed to table: #{chosen_table_name}"
-  end
-
-  def sms_queue(walkin, restaurant, queue_ahead)
-    if queue_ahead == 0
-      p "SMS: Dear #{walkin.name}, your reservation at #{restaurant.name} has been recorded. You are next in line for a party of #{walkin.party_size}. You may start placing your orders at https://locavorusrex.herokuapp.com/restaurants/#{restaurant.id}/menu_items"
-    else
-      p "SMS: Dear #{walkin.name}, your reservation at #{restaurant.name} has been recorded. There is/are #{queue_ahead} customer(s) ahead of you in the queue. You may start placing your orders at https://locavorusrex.herokuapp.com/restaurants/#{restaurant.id}/menu_items"
     end
   end
 
