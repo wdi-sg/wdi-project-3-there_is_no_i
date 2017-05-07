61fd0ea8949b0e1cb4258d8efd368c40593662d1
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-05-08 01:02:20 +0800

    tidy

--- lib/send_twilio.rb
+++ lib/send_twilio.rb
@@ -1,11 +1,8 @@
 module SendTwilio
-  # require 'rubygems'
-  # require 'twilio-ruby'
-
   def send_message(recipient, message)
     twilio_account_sid = ENV['TWILIO_ACCOUNT_SID']
     twilio_auth_token = ENV['TWILIO_AUTH_TOKEN']
-    client = Twilio::REST::Client.new account_sid, auth_token
+    client = Twilio::REST::Client.new twilio_account_sid, twilio_auth_token
     client.account.messages.create(
       :from => "+61428086370", # My Twilio number
       :to => recipient,
