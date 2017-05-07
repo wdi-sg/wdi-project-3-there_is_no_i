515fccf9ad020f391c10d6c0f179b930f7228fb2
Lou Lee <louisa.leeyh@gmail.com>
2017-05-08 00:18:15 +0800

    zhng the restaurants index page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class DashboardController < ApplicationController
   def index
-    @restaurant_id = params[:restaurant_id]
-    @reservations = Reservation.where(restaurant_id: params[:restaurant_id]).order('start_time ASC')
+    @restaurant_id = current_user.restaurant_id
+    puts "current user restaurant id #{@restaurant_id}"
+    @reservations = Reservation.where(restaurant_id: @restaurant_id).order('start_time ASC')
+    @tables = Table.where(restaurant_id: @restaurant_id).order('LOWER(name) ASC')
   end
 end
