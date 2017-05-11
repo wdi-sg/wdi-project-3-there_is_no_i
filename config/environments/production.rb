f36c5beb8c8085ccec3a5557cc8cf25baa63f0f6
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-05-11 09:27:40 +0800

    change email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,23 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
+  # Email
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['GMAIL_USERNAME'],
+   :password             => ENV['GMAIL_PASSWORD'],
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+  }
+
+
+
+
+
   # Devise mailer setting
   config.action_mailer.default_url_options = { host: 'https://locavorusrex.herokuapp.com', port: ENV['PORT'] }
 
---
+++ test/mailers/gmailer_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GmailerMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
