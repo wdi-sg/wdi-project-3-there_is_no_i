b2c4453f95926e9c2875364214ad8cecd432a27d
Darrell Teo <darrelltzj@gmail.com>
2017-05-11 10:24:56 +0800

    sms

--- lib/send_twilio.rb
+++ lib/send_twilio.rb
@@ -15,7 +15,7 @@ module SendTwilio
     if walkin.phone != nil and walkin.phone != ''
       message = "Dear #{walkin.name}, your reservation at #{walkin.restaurant.name} is ready. Please proceed to table: #{walkin.table.name}. Your queue number is #{walkin.queue_number}."
 
-      # send_message('+65' + walkin.phone, message)
+      send_message('+65' + walkin.phone, message)
     end
   end
 
@@ -24,16 +24,16 @@ module SendTwilio
       if queue_ahead == 0
         message = "Dear #{walkin.name}, your reservation at #{walkin.restaurant.name} has been recorded. Your queue number is #{walkin.queue_number}. You are next in line for a cover of #{walkin.party_size}. We will notify you again when your table is ready. In the meantime, you may start placing your orders at https://locavorusrex.herokuapp.com/restaurants/#{walkin.restaurant.id}/menu_items"
 
-        # send_message('+65' + walkin.phone, message)
+        send_message('+65' + walkin.phone, message)
 
       elsif queue_ahead == 1
         message = "Dear #{walkin.name}, your reservation at #{walkin.restaurant.name} has been recorded. Your queue number is #{walkin.queue_number}. There is #{queue_ahead} customer ahead of you in the queue. We will notify you again when your table is ready. In the meantime, you may start placing your orders at https://locavorusrex.herokuapp.com/restaurants/#{walkin.restaurant.id}/menu_items"
 
-        # send_message('+65' + walkin.phone, message)
+        send_message('+65' + walkin.phone, message)
       else
         message = "Dear #{walkin.name}, your reservation at #{walkin.restaurant.name} has been recorded. Your queue number is #{walkin.queue_number}. There are #{queue_ahead} customers ahead of you in the queue. We will notify you again when your table is ready. In the meantime, you may start placing your orders at https://locavorusrex.herokuapp.com/restaurants/#{walkin.restaurant.id}/menu_items"
 
-        # send_message('+65' + walkin.phone, message)
+        send_message('+65' + walkin.phone, message)
       end
     end
   end
@@ -42,7 +42,7 @@ module SendTwilio
     if walkin.phone != nil and walkin.phone != ''
       message = "Dear #{walkin.name}, your table at #{walkin.restaurant.name} has been reassigned. Your new queue number is #{walkin.queue_number}. We will notify you again when the next available table is ready. In the meantime, you may start placing your orders at https://locavorusrex.herokuapp.com/restaurants/#{walkin.restaurant.id}/menu_items"
 
-      # send_message('+65' + walkin.phone, message)
+      send_message('+65' + walkin.phone, message)
     end
   end
 
@@ -50,7 +50,7 @@ module SendTwilio
     if walkin.phone != nil and walkin.phone != ''
       message = "Dear #{walkin.name}, you have been re-queued at #{walkin.restaurant.name}. Your new queue number is #{walkin.queue_number}. We will notify you again when your table is ready. In the meantime, you may start placing your orders at https://locavorusrex.herokuapp.com/restaurants/#{walkin.restaurant.id}/menu_items"
 
-      # send_message('+65' + walkin.phone, message)
+      send_message('+65' + walkin.phone, message)
     end
   end
 
@@ -58,7 +58,7 @@ module SendTwilio
     if walkin.phone != nil and walkin.phone != ''
       message = "Dear #{walkin.name}, we regret to inform you that your reservation at #{walkin.restaurant.name} has been cancelled."
 
-      # send_message('+65' + walkin.phone, message)
+      send_message('+65' + walkin.phone, message)
     end
   end
 
